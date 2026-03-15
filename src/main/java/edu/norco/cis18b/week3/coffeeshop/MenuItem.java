e29b30f2ac291ccea5eb46d58c8c2a4b6a44322b
Ikhlas A. Taj Stanikzai <itajstanik1@student.rccd.edu>
2026-03-15 07:08:40 +0000

    completed the validation part for the constructor

--- src/main/java/edu/norco/cis18b/week3/coffeeshop/MenuItem.java
+++ src/main/java/edu/norco/cis18b/week3/coffeeshop/MenuItem.java
@@ -9,9 +9,24 @@ public class MenuItem {
 
     public MenuItem(String sku, String name, BigDecimal price) {
 
-        if(sku == null || sku.isBlank()) {
+        if (sku == null || sku.isBlank()) {
             throw new IllegalArgumentException("sku cannot be null or blank");
         }
         
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("sku cannot be null or blank");
+        }
+        
+        if (price == null) {
+            throw new IllegalArgumentException("price cannot be null");
+        }
+        
+        if (price.compareTo(BigDecimal.Zero) < 0) {
+            throw new IllegalArgumentException("price must be >= 0");
+
+            this.sku = sku;
+            this.name = name;
+            this.price = price;
+        }
     }
 }
